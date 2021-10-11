9ccf40cbac11570ff0674e69bf8c868cbd8ab00c
hungmv <hungmv@vissoft.vn>
2021-10-11 21:03:35 +0700

    change response

--- src/main/java/com/poly/datn/rest/controler/customer/ProductRest.java
+++ src/main/java/com/poly/datn/rest/controler/customer/ProductRest.java
@@ -1,9 +1,11 @@
 package com.poly.datn.rest.controler.customer;
 
 import com.poly.datn.VO.ProductVO;
+import com.poly.datn.VO.ResponseVO;
 import com.poly.datn.common.Constant;
 import com.poly.datn.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,8 +21,11 @@ public class ProductRest {
 
     // Mah code
     @GetMapping
-    public List<ProductVO> getList(@RequestParam("cate") Optional<Integer> cate, @RequestParam("find") Optional<String> find) {
-        return productService.getList(cate, find);
+    public ResponseEntity<ResponseVO> getList(@RequestParam("cate") Optional<Integer> cate, @RequestParam("find") Optional<String> find) {
+        return ResponseEntity.ok(ResponseVO.builder()
+                .messageCode("200")
+                .messageName("Success")
+                .data(productService.getList(cate, find)).build());
     }
 
     @GetMapping("{id}")
