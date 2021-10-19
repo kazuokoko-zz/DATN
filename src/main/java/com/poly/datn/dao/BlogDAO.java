dc9991e08e831b3eb1cccb4b9c145e3e0a94af78
hungmv <hungmv@vissoft.vn>
2021-10-19 22:12:52 +0700

    update product

--- src/main/java/com/poly/datn/dao/BlogDAO.java
+++ src/main/java/com/poly/datn/dao/BlogDAO.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface BlogDAO extends JpaRepository<Blog, Integer> {
     @Query("select  b from Blog b where b.productId =: productId and b.type =:type")
-    Blog getByProductIdAndType(@Param("productId") Integer productId, @Param("type") Integer type);
+    Optional<Blog> getByProductIdAndType(@Param("productId") Integer productId, @Param("type") Integer type);
 }
