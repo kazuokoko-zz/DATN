eb30473df71340f497e20046ad95e420a207365b
hungmv <hungmv@vissoft.vn>
2021-11-24 00:25:51 +0700

    update

--- src/main/java/com/poly/datn/DatnApplication.java
+++ src/main/java/com/poly/datn/DatnApplication.java
@@ -1,10 +1,15 @@
 package com.poly.datn;
 
 
+import com.poly.datn.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class DatnApplication {
     public static void main(String[] args){
         SpringApplication.run(DatnApplication.class, args);
---
+++ src/main/java/com/poly/datn/config/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.poly.datn.config;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
