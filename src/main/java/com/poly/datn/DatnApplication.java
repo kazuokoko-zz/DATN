05cfbaa6ebf4d2b53c87be73ddff45766dcf7f47
Pham Duc Manh <manhkull999@gmail.com>
2021-11-25 22:39:48 +0700

    update swagger

--- src/main/java/com/poly/datn/DatnApplication.java
+++ src/main/java/com/poly/datn/DatnApplication.java
@@ -5,11 +5,13 @@ import com.poly.datn.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
         FileStorageProperties.class
 })
+@EnableSwagger2
 public class DatnApplication {
     public static void main(String[] args){
         SpringApplication.run(DatnApplication.class, args);
