a353c563e06be39bb9e0db5ba0e40ae81101dfec
hungmv <hungmv@vissoft.vn>
2021-11-27 14:47:59 +0700

    dmk

--- src/main/java/com/poly/datn/DatnApplication.java
+++ src/main/java/com/poly/datn/DatnApplication.java
@@ -3,10 +3,10 @@ package com.poly.datn;
 
 import com.poly.datn.config.FileStorageProperties;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.core.env.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.env.Environment;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Log4j2
@@ -16,21 +16,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 })
 @EnableSwagger2
 public class DatnApplication {
-    public static void main(String[] args){
 
-        Environment env =  SpringApplication.run(DatnApplication.class, args).getEnvironment();
-        String appName = env.getProperty("spring.application.name");
-        if (appName != null) {
-            appName = appName.toUpperCase();
-        }
+    public static void main(String[] args) {
+
+        Environment env = SpringApplication.run(DatnApplication.class, args).getEnvironment();
 
         String port = env.getProperty("server.port");
-        log.info("-------------------------START " + appName
-                + " Application------------------------------");
-        log.info("   Application         : " + appName);
         log.info("   Url swagger-ui      : http://localhost:" + port + "/swagger-ui.html");
-        log.info("-------------------------START SUCCESS " + appName
-                + " Application------------------------------");
+        log.info("-------------------------START SUCCESS------------------------------");
 
     }
 
