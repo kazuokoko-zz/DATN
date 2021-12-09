3484c288b80fd30e35cebc96216a821981ceac35
hungmv <hungmv@vissoft.vn>
2021-12-10 00:43:29 +0700

    update 3/12

--- src/main/java/com/poly/datn/dao/OrdersDAO.java
+++ src/main/java/com/poly/datn/dao/OrdersDAO.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.security.Principal;
-import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
@@ -23,7 +21,7 @@ public interface OrdersDAO extends JpaRepository<Orders, Integer> {
     @Query(nativeQuery = true,value ="select count(*) from orders c where c.date_created between :startTime and  :EndTime")
     Integer countOrdersBy(@Param("startTime") Timestamp startTime,@Param("EndTime") Timestamp EndTime);
 
-    @Query(nativeQuery = true,value ="select count(*) from orders c where c.id in(select order_id from ordermanagement o where o.status = \"Đơn hàng đã hủy\" )and c.date_created between :startTime and  :EndTime")
+    @Query(nativeQuery = true,value ="select count(*) from orders c where c.id in(select order_id from ordermanagement o where o.status = \"Đã hủy\" )and c.date_created between :startTime and  :EndTime")
     Integer countCancerOrdersBy(@Param("startTime") Timestamp startTime,@Param("EndTime") Timestamp EndTime);
 
     @Query(nativeQuery = true,value ="select count(*) from orders c where c.id in(select order_id from ordermanagement o where o.status = \"Giao hàng thành công\" )and c.date_created between :startTime and  :EndTime")
