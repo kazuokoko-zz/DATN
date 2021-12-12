e92dbdf2201928c6c8534c410f87d6ad4d9385be
hungmv <hungmv@vissoft.vn>
2021-12-12 23:03:24 +0700

    update 3/12

--- src/main/java/com/poly/datn/service/impl/OrdersServicesImpl.java
+++ src/main/java/com/poly/datn/service/impl/OrdersServicesImpl.java
@@ -72,7 +72,9 @@ public class OrdersServicesImpl implements OrdersService {
         }
         Orders orders = ordersDAO.findByIdAndUsername(id, principal.getName()).orElseThrow(() -> new SecurityException("Not your order"));
 
-        return getDetailOrders(orders, "Chờ xác nhận");
+        OrdersVO vo = getDetailOrders(orders, "Chờ xác nhận");
+
+        return getStatusLine(vo);
     }
 
     //, OrderDetailsVO orderDetailsVO, CustomerVO customerVO
@@ -113,7 +115,9 @@ public class OrdersServicesImpl implements OrdersService {
                 totalPrice += detailsVO.getQuantity() * (detailsVO.getPrice() - detailsVO.getDiscount());
             }
         }
-
+/**
+ * send order mail
+ */
         infoSendOrder.setDiscount(totalDiscount);
         infoSendOrder.setTotalPrice(totalPrice);
         infoSendOrder.setPrice(price);
@@ -143,7 +147,8 @@ public class OrdersServicesImpl implements OrdersService {
             return null;
         }
         Orders orders = ordersDAO.findById(id).orElseThrow(() -> new SecurityException("Not found"));
-        return getDetailOrders(orders, null);
+        OrdersVO vo = getDetailOrders(orders, null);
+        return getStatusLine(vo);
     }
 
     @Override
@@ -575,5 +580,18 @@ public class OrdersServicesImpl implements OrdersService {
         return orderManagement.getStatus();
     }
 
+    private OrdersVO getStatusLine(OrdersVO ordersVO) {
+        OrdersVO vo = ordersVO;
+        List<OrderManagement> oms = orderManagementDAO.findByOrderId(vo.getId());
+        Collections.sort(oms, Comparator.comparing(OrderManagement::getTimeChange).reversed());
+        List<OrderManagementVO> omvos = new ArrayList<>();
+        for (OrderManagement orderManagement : oms) {
+            OrderManagementVO managementVO = new OrderManagementVO();
+            BeanUtils.copyProperties(orderManagement, managementVO);
+            omvos.add(managementVO);
+        }
+        vo.setOrderManagements(omvos);
+        return vo;
+    }
 
 }
