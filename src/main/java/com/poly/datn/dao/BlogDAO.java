811a611f00b84bb6234fe2d83b8679a28c0fcb93
Pham Duc Manh <manhkull999@gmail.com>
2021-12-16 02:34:35 +0700

    update mail

--- src/main/java/com/poly/datn/dao/BlogDAO.java
+++ src/main/java/com/poly/datn/dao/BlogDAO.java
@@ -10,13 +10,22 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BlogDAO extends JpaRepository<Blog, Integer> {
-    @Query("select  b from Blog b where b.productId =:productId and b.type =:type")
+    @Query("select  b from Blog b where b.productId =:productId and b.type =:type ")
     Optional<Blog> getByProductIdAndType(@Param("productId") Integer productId, @Param("type") Integer type);
 
+    @Query("select  b from Blog b where  b.status =:status")
+    List<Blog> getAllByStatus(@Param("status") Boolean status);
 
-    List<Blog> findAllByProductIdEquals(Integer integer);
+    @Query("select  b from Blog b where b.id =:id and b.status = true")
+    Blog getOneByIdAndStatus(@Param("id") Integer id);
 
-    @Query(value = "select b from Blog b where (b.timeCreated between :start and :end) and b.status = true ")
+    @Query("select  b from Blog b where b.id =:id ")
+    Blog getOneByIdAndStatusAdmin(@Param("id") Integer id);
+
+
+    List<Blog> findAllByProductIdEqualsAndStatus(Integer integer, Boolean status);
+
+    @Query(value = "select b from Blog b where (b.timeCreated between :start and :end) and b.status = true")
     List<Blog> findAllByTimeBetween(@Param("start") Timestamp start,@Param("end") Timestamp end);
 //    List<Blog> findAllInHour();
     Blog findOneById(Integer id);
