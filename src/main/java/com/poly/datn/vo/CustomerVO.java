dc8bbab3a56c648074b8bbc32a80b176a55ebaaf
hungmv <hungmv@vissoft.vn>
2021-12-18 19:02:36 +0700

    update 3/12

--- src/main/java/com/poly/datn/vo/CustomerVO.java
+++ src/main/java/com/poly/datn/vo/CustomerVO.java
@@ -1,18 +1,23 @@
 package com.poly.datn.vo;
 
+import com.poly.datn.common.RegexEmail;
 import lombok.Data;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 
 @Data
-public class CustomerVO {
+public class CustomerVO extends RegexEmail {
 
     private Long id;
-
+    @Size(min = 3)
     private String fullname;
 
+    @Pattern(regexp = RegexEmail.regexE, message = "Email sai")
     private String email;
 
+    @Pattern(regexp = ValidNum,message = "Số điện thoại sai")
     private String phone;
 
     private String address;
