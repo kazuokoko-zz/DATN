d10686bad180027d3fd609930786d83ecf329c2f
hungmv <hungmv@vissoft.vn>
2021-12-20 17:46:53 +0700

    update 3/12

--- src/main/java/com/poly/datn/dao/ProductColorDAO.java
+++ src/main/java/com/poly/datn/dao/ProductColorDAO.java
@@ -2,7 +2,9 @@ package com.poly.datn.dao;
 
 import com.poly.datn.entity.ProductColor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,10 +13,14 @@ public interface ProductColorDAO extends JpaRepository<ProductColor, Integer> {
 
     List<ProductColor> findAllByProductIdEquals(Integer id);
 
-   ProductColor findByColorId(Integer id);
+    ProductColor findByColorId(Integer id);
 
-   ProductColor findByProductIdAndColorId(Integer productId,Integer colorId);
+    ProductColor findByProductIdAndColorId(Integer productId, Integer colorId);
 
 
     void deleteAllByProductIdEquals(Integer productId);
+
+    @Query("select sum(p.quantity) as cou from ProductColor p where  p.productId = :pid")
+    Integer getNumerProductOf(@Param("pid") Integer pid);
+
 }
