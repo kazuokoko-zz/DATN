99a36c89578c464fc38a59ee4050752e3eea449f
Pham Duc Manh <manhkull999@gmail.com>
2021-12-20 23:53:04 +0700

    update mail

--- src/main/java/com/poly/datn/vo/SaleVO.java
+++ src/main/java/com/poly/datn/vo/SaleVO.java
@@ -16,8 +16,8 @@ public class SaleVO extends RegexEmail {
 
 
     private Integer id;
-    @Pattern(regexp = ValidName, message = "tên sai định dạng")
     @Size(max = DISPLAY_NAME_MAX_LENGTH, min = DISPLAY_NAME_MIN_LENGTH, message = "tên tối thiểu 5 ký tự và tối đa 100 ký tự")
+    @Pattern(regexp = ValidName, message = "tên sai định dạng")
     private String name;
     @NotNull
     private Timestamp startTime;
